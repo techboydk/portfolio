28a3c227b94c371a7f69e1712aed65abbe045476
techboydk <003yadavdipesh@gmail.com>
2023-11-14 14:05:47 +0530

    wip

---
+++ src/assets/data/skills.js
@@ -0,0 +1,31 @@
+export const Skills = [
+    {
+        icon: "fa-brands fa-html5",
+        skillName: "html"
+    },
+    {
+        icon: "fa-brands fa-css3-alt",
+        skillName: "css"
+    },
+    {
+        icon: "fa-brands fa-sass",
+        skillName: "sass"
+    },
+    {
+        icon: "fa-brands fa-bootstrap",
+        skillName: "bootstrap"
+    },
+    {
+        icon: "fa-brands fa-js",
+        skillName: "javascript"
+    },
+    {
+        icon: "fa-brands fa-react",
+        skillName: "react"
+    },
+    {
+        icon: "fa-brands fa-node",
+        skillName: "nodejs"
+    },
+
+]
--- src/components/Main/Skill/Skill.jsx
+++ src/components/Main/Skill/Skill.jsx
@@ -1,26 +1,22 @@
-import React from 'react'
-import Card from '../../Card/Card'
-import SectionHeading from '../../SectionHeading/SectionHeading'
-import './skill.scss'
-import { Skills } from './skills'
+import React from "react";
+import Card from "../../Card/Card";
+import SectionHeading from "../../SectionHeading/SectionHeading";
+import "./skill.scss";
+import { Skills } from "../../../assets/data/skills";
 
 const Skill = () => {
   return (
-    <div className="skill-section" id='skills'>
-        <SectionHeading heading="my skills" bgText="skills"/>
-        <div className="skill-wrapper">
-            <div className="cards">
-                {
-                    Skills.map((item)=>{
-                        return(
-                            <Card skill={item} key={item.skillName} />
-                        )
-                    })
-                }
-            </div>
+    <div className="skill-section" id="skills">
+      <SectionHeading heading="my skills" bgText="skills" />
+      <div className="skill-wrapper">
+        <div className="cards">
+          {Skills.map((item) => {
+            return <Card skill={item} key={item.skillName} />;
+          })}
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skill
+export default Skill;
